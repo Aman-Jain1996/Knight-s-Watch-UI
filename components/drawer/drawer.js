d1876fc469670aa285cdc30106c3a772c5f7439d
[Aman Jain] <[ajain8479@gmail.com]>
2022-04-19 11:42:58 +0530

    Added drawer component

---
+++ components/drawer/drawer.js
@@ -0,0 +1,29 @@
+const menuIcon = document.querySelector(".menu-icon");
+const closeIcon = document.querySelector(".close-icon");
+
+function showMenu() {
+  document
+    .querySelector(".dismissible-drawer-menu")
+    .classList.remove("menu-hidden");
+
+  document
+    .querySelector(".dismissible-main-content")
+    .classList.remove("expanded-main-content");
+  menuIcon.style.display = "none";
+}
+
+menuIcon.addEventListener("click", showMenu);
+
+function HideMenu() {
+  console.log();
+  document
+    .querySelector(".dismissible-drawer-menu")
+    .classList.add("menu-hidden");
+
+  document
+    .querySelector(".dismissible-main-content")
+    .classList.add("expanded-main-content");
+  menuIcon.style.display = "block";
+}
+
+closeIcon.addEventListener("click", HideMenu);
